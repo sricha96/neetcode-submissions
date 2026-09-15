990b41877b8be7f95664678a1cd2c110e1f1935f
Smriti Richa <smritiricha96@gmail.com>
2026-09-15 17:11:05 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] height) {
+        if(height == null){
+            return -1;
+        }
+        int leftMax = 0; 
+        int rightMax = 0;
+        int l = 0;
+        int r = height.length - 1;
+        int res = 0;
+        while(l < r){
+            leftMax = Math.max(leftMax, height[l]);
+            rightMax = Math.max(rightMax, height[r]);
+            if(leftMax < rightMax){
+                res = res + (leftMax - height[l]);
+                l++;
+            }else{
+                res = res + (rightMax - height[r]);
+                r--;
+            }
+        }
+        return res;
+    }
+}
